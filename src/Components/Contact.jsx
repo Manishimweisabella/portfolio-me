2f5ecd2b64895522495f00c03741ee329ceab2f1
manishimwe Isabella <manishimweisabella@gmail.com>
2025-05-09 16:01:33 +0200

    this is my contact

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
-import { FaCity } from "react-icons/fa";
+
 
 
 const Contact = () => {
   return (
-    <div className="py-12 px-6 bg-black min-h-[24rem]" id="contact">
+    <div className="py-12 px-6 bg-black h-screen" id="contact">
       <h2 className="text-3xl text-white font-semibold text-center mb-8 hover:text-gray-500">
         Contact Me
       </h2>
-    <div className='mt-4'> 
-      <FaCity><span>street,city,country</span></FaCity> </div>
+   
+     
       <div className="max-w-md mx-auto p-6 rounded-xl shadow-md space-y-4 bg-gray-900">
         <form className="space-y-4">
           <div className="flex flex-col text-left">
@@ -17,15 +17,15 @@ const Contact = () => {
             <input
               type="email"
               placeholder="Enter your email"
-              className="bg-slate-800 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="bg-slate-400 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
             />
           </div>
 
           <div className="flex flex-col text-left">
-            <label className=" border-gray-600 text-white">Message</label>
+            <label className=" border-gray-400 text-white">Message</label>
             <textarea
               placeholder="Enter your message"
-              className="bg-gray-800 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="bg-gray-400 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
               rows="4"
             />
           </div>
@@ -35,7 +35,7 @@ const Contact = () => {
             <input
               type="tel"
               placeholder="Enter your telephone"
-              className="bg-slate-800 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
+              className="bg-slate-400 px-4 py-2 border border-gray-300 rounded-lg"
             />
           </div>
 
